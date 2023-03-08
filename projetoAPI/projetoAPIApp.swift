56b439fb47ece91c2a695c0f981fd0d43122afc3
Arthur Soares <arthursoares.js@gmail.com>
2023-03-08 17:32:18 -0300

    Initial Commit

---
+++ projetoAPI/projetoAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  projetoAPIApp.swift
+//  projetoAPI
+//
+//  Created by Arthur Soares on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct projetoAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
